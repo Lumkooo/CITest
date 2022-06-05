52c3f33b595bd638d971ff986562975862b0b1ea
Lumkooo <shaminandrey2013@gmail.com>
2022-06-05 12:06:11 +0700

    Initial commit

---
+++ CITestApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CITestApp
+//
+//  Created by Андрей Шамин on 6/4/22.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
