85137ff07baa703d8803af7495a877e86c47b9d7
Lumkooo <shaminandrey2013@gmail.com>
2022-06-18 12:13:04 +0700

    some changes(for test)

--- CITestApp/ViewControllers/Main/Views/MainView.swift
+++ CITestApp/ViewControllers/Main/Views/MainView.swift
@@ -22,7 +22,7 @@ final class MainView: UIView {
     }
 
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
 }
 
